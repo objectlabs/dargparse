d46e4a66aa1c5a1211a00a1ac90691cfc65b82bb
abdul <abdul@mongolab.com>
2012-06-09 02:24:00 -0700

    Setup info update

--- setup.py
+++ setup.py
@@ -26,13 +26,13 @@ from setuptools import setup
 setup(
     name='dargparse',
     version='0.1.0',
-    author='ObjectLabs staff',
-    author_email='staff@objectlabs.com',
+    author='The Mongolab team',
+    author_email='team@mongolab.com',
     description='Declarative command-line argument parser for python',
     long_description="Declarative command-line argument parser for python",
     packages=['dargparse'],
     test_suite="dargparse.tests.test_suite",
-    url='http://objectlabs.org',
+    url='http://mongolab.com',
     license='LICENSE.txt',
     install_requires=[
         'argparse==1.2.1'
