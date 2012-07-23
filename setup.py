65e1d9e3c1b2a4c99670160a7cadda2697380e2a
abdul <abdul@mongolab.com>
2012-07-23 15:15:35 -0700

    Increment version to 0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 setup(
     name='dargparse',
-    version='0.1.0',
+    version='0.2.0',
     author='The MongoLab Team',
     author_email='team@mongolab.com',
     description='Declarative command-line argument parser for python',
